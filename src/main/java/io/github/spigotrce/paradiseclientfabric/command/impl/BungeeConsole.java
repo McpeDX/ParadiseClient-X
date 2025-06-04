9f09dc513692ce863c00034a439e0365cec44afe
McpeDX <164185653+McpeDX@users.noreply.github.com>
2025-06-04 08:37:35 +0530

    Update BungeeConsole.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/BungeeConsole.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/BungeeConsole.java
@@ -1,15 +1,14 @@
 package io.github.spigotrce.paradiseclientfabric.command.impl;
 
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.github.spigotrce.paradiseclientfabric.Helper;
-import net.minecraft.CommandSource;
-import net.minecraft.Packet;
-import net.minecraft.CustomPayloadC2SPacket;
-import net.minecraft.MinecraftClient;
-import net.minecraft.CustomPayload;
+import io.github.spigotrce.paradiseclientfabric.command.Command;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.server.command.ServerCommandSource;
 
 public class BungeeConsole extends Command {
     public BungeeConsole(MinecraftClient minecraftClient) {
@@ -17,15 +16,21 @@ public class BungeeConsole extends Command {
     }
 
     @Override
-    public LiteralArgumentBuilder<CommandSource> build() {
-        return (BungeeConsole.literal(this.getName()).executes(context -> {
-            Helper.printChatMessage("Incomplete command!");
-            return 1;
-        })).then(BungeeConsole.argument("command", StringArgumentType.greedyString()).executes(this::build));
+    public LiteralArgumentBuilder<ServerCommandSource> build() {
+        return LiteralArgumentBuilder.<ServerCommandSource>literal(this.getName())
+                .executes(context -> {
+                    Helper.printChatMessage("Incomplete command!");
+                    return 1;
+                })
+                .then(com.mojang.brigadier.builder.RequiredArgumentBuilder
+                        .<ServerCommandSource, String>argument("command", StringArgumentType.greedyString())
+                        .executes(this::build)
+                );
     }
 
     private int build(CommandContext<?> context) {
-        Helper.sendPacket(new CustomPayloadC2SPacket(new BungeeCommandWriter(context.getArgument("command", String.class))));
+        String command = StringArgumentType.getString(context, "command");
+        Helper.sendPacket(new CustomPayloadC2SPacket(new BungeeCommandWriter(command)));
         Helper.printChatMessage("Payload sent!");
         return 1;
     }

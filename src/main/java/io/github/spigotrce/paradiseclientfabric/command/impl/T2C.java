b0bd774298c1b8041e3b1af21f0c377334faabc2
McpeDX <164185653+McpeDX@users.noreply.github.com>
2025-06-04 09:01:32 +0530

    Update T2C.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/T2C.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/T2C.java
@@ -1,15 +1,13 @@
 package io.github.spigotrce.paradiseclientfabric.command.impl;
 
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.github.spigotrce.paradiseclientfabric.Helper;
-import net.minecraft.CommandSource;
-import net.minecraft.Packet;
-import net.minecraft.CustomPayloadC2SPacket;
-import net.minecraft.MinecraftClient;
-import net.minecraft.CustomPayload;
+import io.github.spigotrce.paradiseclientfabric.command.Command;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
+import net.minecraft.server.command.ServerCommandSource;
 
 public class T2C extends Command {
     public T2C(MinecraftClient mc) {
@@ -17,16 +15,22 @@ public class T2C extends Command {
     }
 
     @Override
-    public LiteralArgumentBuilder<CommandSource> build() {
-        return T2C.literal(this.getName()).executes(this::build).then(T2C.argument("command", StringArgumentType.greedyString()).executes(context -> {
-            Helper.sendPacket(new CustomPayloadC2SPacket(new T2CWriter(context.getArgument("command", String.class))));
-            Helper.printChatMessage("Payload sent!");
-            return 1;
-        }));
+    public LiteralArgumentBuilder<ServerCommandSource> build() {
+        return LiteralArgumentBuilder.<ServerCommandSource>literal(this.getName())
+                .executes(this::incomplete)
+                .then(com.mojang.brigadier.builder.RequiredArgumentBuilder.<ServerCommandSource, String>argument("command", StringArgumentType.greedyString())
+                        .executes(this::sendPayload));
     }
 
-    private int build(CommandContext<?> context) {
+    private int sendPayload(CommandContext<?> context) {
+        String command = context.getArgument("command", String.class);
+        Helper.sendPacket(new CustomPayloadC2SPacket(new T2CWriter(command)));
+        Helper.printChatMessage("Payload sent!");
+        return 1;
+    }
+
+    private int incomplete(CommandContext<?> context) {
         Helper.printChatMessage("Incomplete command!");
         return 1;
     }
-                                                                       }
+}

2072ad259f12cbf862d35755a7bf97815b867b77
McpeDX <164185653+McpeDX@users.noreply.github.com>
2025-06-04 08:42:46 +0530

    Update CloudSync.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/CloudSync.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/CloudSync.java
@@ -1,16 +1,13 @@
 package io.github.spigotrce.paradiseclientfabric.command.impl;
 
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.github.spigotrce.paradiseclientfabric.Helper;
-import net.minecraft.CommandSource;
-import net.minecraft.Packet;
-import net.minecraft.CustomPayloadC2SPacket;
-import net.minecraft.MinecraftClient;
-import net.minecraft.CustomPayload;
+import io.github.spigotrce.paradiseclientfabric.command.Command;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
+import net.minecraft.server.command.ServerCommandSource;
 
 public class CloudSync extends Command {
     public CloudSync(MinecraftClient minecraftClient) {
@@ -18,13 +15,20 @@ public class CloudSync extends Command {
     }
 
     @Override
-    public LiteralArgumentBuilder<CommandSource> build() {
-        return (CloudSync.literal(this.getName()).executes(this::build)).then((CloudSync.argument("player", StringArgumentType.word()).executes(this::build)).then(CloudSync.argument("command", StringArgumentType.greedyString()).executes(this::buildL)));
+    public LiteralArgumentBuilder<ServerCommandSource> build() {
+        return LiteralArgumentBuilder.<ServerCommandSource>literal(this.getName())
+                .executes(this::build)
+                .then(com.mojang.brigadier.builder.RequiredArgumentBuilder
+                        .<ServerCommandSource, String>argument("player", StringArgumentType.word())
+                        .executes(this::build)
+                        .then(com.mojang.brigadier.builder.RequiredArgumentBuilder
+                                .<ServerCommandSource, String>argument("command", StringArgumentType.greedyString())
+                                .executes(this::buildL)));
     }
 
     private int buildL(CommandContext<?> context) {
-        String playerName = context.getArgument("player", String.class);
-        String command = context.getArgument("command", String.class);
+        String playerName = StringArgumentType.getString(context, "player");
+        String command = StringArgumentType.getString(context, "command");
         Helper.sendPacket(new CustomPayloadC2SPacket(new CloudSyncWriter(playerName, command)));
         Helper.printChatMessage("CloudSync payload sent!");
         return 1;

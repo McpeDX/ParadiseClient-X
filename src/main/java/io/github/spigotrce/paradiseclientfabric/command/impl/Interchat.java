be578758f9cd6545304c6198f4382e6ffc290d7a
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-06-07 21:39:55 +0530

    Update Interchat.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/Interchat.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/Interchat.java
@@ -2,7 +2,6 @@ package io.github.spigotrce.paradiseclientfabric.command.impl;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
@@ -10,10 +9,16 @@ import io.github.spigotrce.paradiseclientfabric.Helper;
 import io.github.spigotrce.paradiseclientfabric.command.Command;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.command.CommandSource;
 import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
-import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.util.Identifier;
+import net.minecraft.network.PacketByteBuf;
+import io.netty.buffer.Unpooled;
 
 import java.util.concurrent.CompletableFuture;
+
+import static net.minecraft.client.command.CommandManager.literal;
+import static net.minecraft.client.command.CommandManager.argument;
 
 public class Interchat extends Command {
     public Interchat(MinecraftClient minecraftClient) {
@@ -21,37 +26,46 @@ public class Interchat extends Command {
     }
 
     @Override
-    public LiteralArgumentBuilder<ServerCommandSource> build() {
-        return LiteralArgumentBuilder.<ServerCommandSource>literal(this.getName())
-                .executes(this::incomplete)
-                .then(RequiredArgumentBuilder.<ServerCommandSource, String>argument("user", StringArgumentType.word())
-                        .suggests(this::suggestPlayers)
-                        .executes(this::incomplete)
-                        .then(RequiredArgumentBuilder.<ServerCommandSource, String>argument("command", StringArgumentType.greedyString())
-                                .executes(this::sendPayload)));
+    public LiteralArgumentBuilder<CommandSource> build() {
+        return literal(getName())
+            .executes(this::showUsage)
+            .then(argument("user", StringArgumentType.word())
+                .suggests(this::suggestPlayers)
+                .executes(this::showUsage)
+                .then(argument("command", StringArgumentType.greedyString())
+                    .executes(this::executeCommand)));
     }
 
-    private int sendPayload(CommandContext<?> context) {
+    private int executeCommand(CommandContext<CommandSource> context) {
         String user = StringArgumentType.getString(context, "user");
         String command = StringArgumentType.getString(context, "command");
 
-        for (PlayerListEntry p : this.getMinecraftClient().getNetworkHandler().getPlayerList()) {
-            if (p.getProfile().getName().equalsIgnoreCase(user)) {
-                String uuid = p.getProfile().getId().toString();
-                Helper.sendPacket(new CustomPayloadC2SPacket(new InterchatWriter(uuid, command)));
-                Helper.printChatMessage("Payload sent!");
+        for (PlayerListEntry player : getMinecraftClient().getNetworkHandler().getPlayerList()) {
+            if (player.getProfile().getName().equalsIgnoreCase(user)) {
+                String uuid = player.getProfile().getId().toString();
+                
+                // Create proper payload
+                PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+                buf.writeString(uuid);
+                buf.writeString(command);
+                
+                Identifier channel = new Identifier("interchat", "command");
+                CustomPayloadC2SPacket packet = new CustomPayloadC2SPacket(channel, buf);
+                
+                Helper.sendPacket(packet);
+                Helper.printChatMessage("Payload sent to " + user + "!");
                 return 1;
             }
         }
 
-        Helper.printChatMessage("Player not found!");
+        Helper.printChatMessage("Player " + user + " not found!");
         return 1;
     }
 
-    private CompletableFuture<Suggestions> suggestPlayers(CommandContext<?> context, SuggestionsBuilder builder) {
+    private CompletableFuture<Suggestions> suggestPlayers(CommandContext<CommandSource> context, SuggestionsBuilder builder) {
         String partial = builder.getRemaining().toLowerCase();
-        this.getMinecraftClient().getNetworkHandler().getPlayerList().forEach(entry -> {
-            String name = entry.getProfile().getName();
+        getMinecraftClient().getNetworkHandler().getPlayerList().forEach(player -> {
+            String name = player.getProfile().getName();
             if (name.toLowerCase().startsWith(partial)) {
                 builder.suggest(name);
             }
@@ -59,8 +73,8 @@ public class Interchat extends Command {
         return builder.buildFuture();
     }
 
-    private int incomplete(CommandContext<?> context) {
-        Helper.printChatMessage("Incomplete command!");
+    private int showUsage(CommandContext<CommandSource> context) {
+        Helper.printChatMessage("Usage: .interchat <user> <command>");
         return 1;
     }
 }

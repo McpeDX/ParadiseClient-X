6d897c436344cc09ed98b22efacd639f588fc51e
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-06-07 21:35:19 +0530

    Update BungeeConsole.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/BungeeConsole.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/BungeeConsole.java
@@ -3,15 +3,17 @@ package io.github.spigotrce.paradiseclientfabric.command.impl;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import io.github.spigotrce.paradiseclientfabric.Helper;
 import io.github.spigotrce.paradiseclientfabric.command.Command;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.util.Identifier;
+import net.minecraft.command.CommandSource;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
+import net.minecraft.util.Identifier;
 import io.netty.buffer.Unpooled;
+
+import static net.minecraft.client.command.CommandManager.literal;
+import static net.minecraft.client.command.CommandManager.argument;
 
 public class BungeeConsole extends Command {
     public BungeeConsole(MinecraftClient minecraftClient) {
@@ -19,25 +21,23 @@ public class BungeeConsole extends Command {
     }
 
     @Override
-    public LiteralArgumentBuilder<ServerCommandSource> build() {
-        return LiteralArgumentBuilder.<ServerCommandSource>literal(this.getName())
-                .executes(context -> {
-                    Helper.printChatMessage("Usage: .atlas <command>");
-                    return 1;
-                })
-                .then(RequiredArgumentBuilder
-                        .<ServerCommandSource, String>argument("command", StringArgumentType.greedyString())
-                        .executes(this::sendCommand)
-                );
+    public LiteralArgumentBuilder<CommandSource> build() {
+        return literal(getName())
+            .executes(context -> {
+                Helper.printChatMessage("Usage: .atlas <command>");
+                return 1;
+            })
+            .then(argument("command", StringArgumentType.greedyString())
+                .executes(this::sendCommand);
     }
 
-    private int sendCommand(CommandContext<?> context) {
+    private int sendCommand(CommandContext<CommandSource> context) {
         String command = StringArgumentType.getString(context, "command");
 
         PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-        buf.writeString(command); // Customize format based on Bungee plugin
+        buf.writeString(command);
 
-        Identifier channel = new Identifier("bungeecord", "main"); // Common BungeeCord plugin message channel
+        Identifier channel = new Identifier("bungeecord", "main");
         CustomPayloadC2SPacket packet = new CustomPayloadC2SPacket(channel, buf);
 
         Helper.sendPacket(packet);

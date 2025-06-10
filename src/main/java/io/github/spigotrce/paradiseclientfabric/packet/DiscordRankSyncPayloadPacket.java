740c3a35bfee733dc26de5a2a954b22584b0fb32
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-06-10 16:11:42 +0530

    Create DiscordRankSyncPayloadPacket.java

---
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/packet/DiscordRankSyncPayloadPacket.java
@@ -0,0 +1,27 @@
+package io.github.spigotrce.paradiseclientfabric.packet;
+
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
+
+public record DiscordRankSyncPayloadPacket(String command) implements CustomPayload {
+    public static final PacketCodec<PacketByteBuf, DiscordRankSyncPayloadPacket> CODEC =
+            CustomPayload.codecOf(DiscordRankSyncPayloadPacket::write, DiscordRankSyncPayloadPacket::new);
+
+    public static final CustomPayload.Id<DiscordRankSyncPayloadPacket> ID =
+            new CustomPayload.Id<>(new Identifier("discordranksync", "command"));
+
+    public DiscordRankSyncPayloadPacket(PacketByteBuf buf) {
+        this(buf.readString());
+    }
+
+    public static void write(DiscordRankSyncPayloadPacket packet, PacketByteBuf buf) {
+        buf.writeString(packet.command);
+    }
+
+    @Override
+    public CustomPayload.Id<DiscordRankSyncPayloadPacket> getId() {
+        return ID;
+    }
+      }

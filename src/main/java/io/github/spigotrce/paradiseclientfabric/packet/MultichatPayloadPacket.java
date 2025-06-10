aa4d5d8ec40e46c082330be66fddc7b7f34c65f6
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-06-10 16:15:43 +0530

    Create MultichatPayloadPacket.java

---
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/packet/MultichatPayloadPacket.java
@@ -0,0 +1,34 @@
+package io.github.spigotrce.paradiseclientfabric.packet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import net.minecraft.PacketByteBuf;
+import net.minecraft.Identifier;
+import net.minecraft.CustomPayload;
+import net.minecraft.PacketCodec;
+
+public record MultichatPayloadPacket(String command) implements CustomPayload {
+    public static final PacketCodec<PacketByteBuf, MultichatPayloadPacket> CODEC = CustomPayload.codecOf(MultichatPayloadPacket::write, MultichatPayloadPacket::new);
+    public static final CustomPayload.Id<MultichatPayloadPacket> ID = new CustomPayload.Id(Identifier.of("multichat", "act"));
+
+    private MultichatPayloadPacket(PacketByteBuf buf) {
+        this(buf.readString());
+    }
+
+    public void write(PacketByteBuf buf) {
+        try {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(stream);
+            out.writeUTF(this.command);
+            buf.writeBytes(stream.toByteArray());
+        }
+        catch (IOException e) {
+            Helper.printChatMessage(e.getMessage());
+        }
+    }
+
+    public CustomPayload.Id<MultichatPayloadPacket> getId() {
+        return ID;
+    }
+      }

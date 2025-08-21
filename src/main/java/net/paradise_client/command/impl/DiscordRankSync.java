8d8a580a08a955e12f31357329a630f289bd89db
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-08-21 07:35:56 +0530

    Update DiscordRankSync.java

--- src/main/java/net/paradise_client/command/impl/DiscordRankSync.java
+++ src/main/java/net/paradise_client/command/impl/DiscordRankSync.java
@@ -1,13 +1,11 @@
 package net.paradise_client.command.impl;
 
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import net.paradise_client.Helper;
 import net.minecraft.command.CommandSource;
 import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
-import net.minecraft.client.MinecraftClient;
 import net.paradise_client.command.Command;
 import net.paradise_client.packet.DiscordRankSyncPacket;
 
@@ -18,14 +16,22 @@ public class DiscordRankSync extends Command {
 
     @Override
     public LiteralArgumentBuilder<CommandSource> build() {
-        return (DiscordRankSync.literal(this.getName()).executes(this::build)).then(DiscordRankSync.argument("command", StringArgumentType.greedyString()).executes(context -> {
-            Helper.sendPacket(new CustomPayloadC2SPacket(new DiscordRankSyncPacket(context.getArgument("command", String.class))));
-            Helper.printChatMessage("Payload sent!");
-            return 1;
-        }));
+        return LiteralArgumentBuilder.<CommandSource>literal(this.getName())
+            .executes(this::incompleteCommand)
+            .then(
+                com.mojang.brigadier.builder.RequiredArgumentBuilder
+                    .<CommandSource, String>argument("command", StringArgumentType.greedyString())
+                    .executes(context -> {
+                        Helper.sendPacket(new CustomPayloadC2SPacket(
+                            new DiscordRankSyncPacket(context.getArgument("command", String.class))
+                        ));
+                        Helper.printChatMessage("Payload sent!");
+                        return 1;
+                    })
+            );
     }
 
-    private int build(CommandContext<?> context) {
+    private int incompleteCommand(CommandContext<CommandSource> context) {
         Helper.printChatMessage("Incomplete command!");
         return 1;
     }

